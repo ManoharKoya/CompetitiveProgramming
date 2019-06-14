56f74e2c027396553fea9524a2bfb0425654280b
unknown <koyasairammanohar@gmail.com>
2019-06-15 02:42:41 +0530

    fub in logn

---
+++ fibonacci-logn.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#define lli long long int
+#define NA(i,n) for(lli i=0;i<n;i++)
+
+using namespace std;
+lli **arr matrixMul(lli a[][], lli b[][]){
+    
+}
+int main(){
+    return 0;
+}
