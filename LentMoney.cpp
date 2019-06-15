bbe77d1950a42f1c9713789b623c18dba67e050a
unknown <koyasairammanohar@gmail.com>
2019-06-15 19:45:55 +0530

    lazy commit nothing here

---
+++ LentMoney.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<iterator>
+#define lli long long int
+#define MOD 1000000007
+#define pb push_back
+#define NA(i,n) for(lli i=0;i<n;i++)
+using namespace std;
+void solve(){
+    lli n; cin>>n; 
+}
+int main(){
+    lli t; cin>>t;
+    NA(i,t) solve();
+    return 0;
+}
