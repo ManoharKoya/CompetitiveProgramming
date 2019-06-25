a8c55c47b66b6d30b828ffaa495901b667cd04e3
unknown <koyasairammanohar@gmail.com>
2019-06-25 10:14:13 +0530

    f-Faces,t-Turns,s-Sum totalWays

---
+++ faces-turns-sum.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<iterator>
+#define lli long long int
+#define MOD 1000000007
+#define pb push_back
+#define INF 10000000007
+#define NA(i,s,n) for(lli i=s;i<n;i++)
+using namespace std;
+lli dp[51][51];
+lli totWays(lli f,lli t,lli s){
+    if(t==0 && s==0) return 1;
+    if(t<0 || s<0 || f*t<s || t>s ) return 0;
+    if(dp[t][s]==0)
+    NA(i,1,f+1) dp[t][s]+=totWays(f,t-1,s-i);
+    return dp[t][s];
+}
+int main(){
+    lli F,T,S; cin>>F>>T>>S;
+    NA(i,0,T+1){ NA(j,0,S+1) dp[i][j]=0; }
+    cout<<totWays(F,T,S)<<endl;
+    return 0;
+}
