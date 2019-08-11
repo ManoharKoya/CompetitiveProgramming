7dfb05b70f83295bf1e349ce33302d1480868332
unknown <koyasairammanohar@gmail.com>
2019-08-11 14:57:28 +0530

    topdown memoistion

--- subSetSum.cpp
+++ subSetSum.cpp
@@ -13,16 +13,25 @@
 #define fastIO ios_base::sync_with_stdio(false); cin.tie(NULL)
 #define NA(i,s,n) for(lli i=s;i<n;i++)
 using namespace std;
+vector<vector<lli> > lookUp;  // memoisation approach.
 bool findSum(lli a[],lli n,lli s){
     if(s==0) return true;
     else if(s<0 || n<0) return false;
+    if(lookUp[n][s]==-1)
+    {
     bool inc = findSum(a,n-1,s-a[n]);
     bool exc = findSum(a,n-1,s);
-    return inc||exc;
+    lookUp[n][s] = inc||exc;
+    }
+    return lookUp[n][s];
 }
 void solve(){
     fastIO;
     lli n; cin>>n;
+    lookUp.resize(queen);
+    NA(i,0,1000){
+        lookUp[i].resize(1000,-1);
+    }
     lli a[n]; NA(i,0,n) cin>>a[i];
     lli s; cin>>s;
     cout<<findSum(a,n-1,s)<<endl;
