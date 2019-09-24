77b33d23cf8215c4bb3202badd1e8c7e466b5014
unknown <koyasairammanohar@gmail.com>
2019-09-24 23:44:46 +0530

    modified

--- inFolder/medianFind-O(n)-avg.cpp
+++ inFolder/medianFind-O(n)-avg.cpp
@@ -13,6 +13,37 @@
 #define fastIO ios_base::sync_with_stdio(false); cin.tie(NULL)
 #define NA(i,s,n) for(lli i=s;i<n;i++)
 using namespace std;
+using namespace std::chrono;
+
+/*
+Prerequisites.
+    -> kth smallest element in an array in O(n). 
+    -> QuickSelect algo.
+    -> median find in O(n).
+
+Pseudocode.
+
+1. create (val/wt) array.
+2. find median(val/wt) in array.
+procedure(median,W):
+    3. cal. sum of wt's of (val/wt)>median.
+    4. if(W1==W) return median.
+        else if(W1>W) newMed = median((vla/wt) < median), return procedure(newMed,W-M);
+        else newMed = median((val/wt)>median), return procedure(newMed,W);
+
+
+This whill only ensure that average time of O(n).
+For worstcase O(n) -> check median-of-medians Algo.
+
+ex output :
+$ ./a
+100000
+16459
+16000 ms <- O(N) avg case
+16459
+47114 ms <- O(NlogN)
+
+*/
 
 lli medianFind(vector<lli> a,lli k){
     if(a.size()==1 || k==0) return a[0];
@@ -27,7 +58,20 @@ lli medianFind(vector<lli> a,lli k){
 int main(){
     lli n; cin>>n;
     vi a(n);
-    NA(i,0,n) cin>>a[i];
-    cout<<medianFind(a,n/2);
+    NA(i,0,n) 
+        a[i] = rand();
+    auto start = high_resolution_clock::now();
+    cout<<medianFind(a,n/2),nl;
+    auto end = high_resolution_clock::now();
+    auto dur = duration_cast<microseconds>(end-start);
+    cout<<dur.count()<<" ms <- O(N) avg case",nl;
+
+    start = high_resolution_clock::now();
+    sort(a.begin(),a.end());
+    cout<<a[n/2],nl;
+    end = high_resolution_clock::now();
+    dur = duration_cast<microseconds>(end-start);
+    cout<<dur.count()<<" ms <- O(NlogN)",nl;
+
     return 0;
 }
