e1af5c29ea6ae7ba57ee5290464a0eb413f76e52
unknown <koyasairammanohar@gmail.com>
2019-11-23 19:59:06 +0530

    trie implementation..

--- trie.cpp
+++ trie.cpp
@@ -1,54 +1,68 @@
-// How many numbers x are there in the range a to b, where the digit d occurs exactly k times in x? 
 #include<bits/stdc++.h>
 #define lli long long int
 #define MOD 1000000007
-#define KING 1000000000
-#define queen 1000009
+#define KING 1000000002
+#define queen 1000002
+#define fi first
+#define se second
+#define nl cout<<"\n"
+#define TEST cout<<"TEST",nl
 #define pr pair<lli,lli>
 #define pb push_back
 #define eb emplace_back
 #define mp make_pair
 #define vi vector<lli>
 #define vpi vector<pr> 
+#define P pair<lli,pr>
 #define fastIO ios_base::sync_with_stdio(false); cin.tie(NULL)
 #define NA(i,s,n) for(lli i=s;i<n;i++)
 using namespace std;
+/*
+  complexity analysis.. 
+    insertion O(n) n-length of string.
+    searching O(n) n-length of string.
+*/
 struct trie{
-  lli f; bool end;
-  trie *children[27];
+  struct trie *cld[26];
+  bool EndofStr;
 };
-struct trie* root = NULL;
-struct trie* newnode(){
-  struct trie* t = (struct trie*)malloc(sizeof(struct trie));
-  t->f=0; 
-  NA(i,0,27) {
-      t->children[i] = (struct trie*)malloc(sizeof(struct trie));
-      t->children[i]->f=0;
-  }
+struct trie* getTrie(){
+  struct trie *t = new trie;
+  t->EndofStr = false;
+  NA(i,0,26) t->cld[i]=NULL;
   return t;
 }
-void insert(string s){
-  struct trie *t = root;
-  NA(i,0,s.length()-1){
-    t->children[s[i]-'a']->f++;
-    if(t->children[s[i]='a']->end) 
-    t=t->children[s[i]-'a'];
+void insert(struct trie* root, string s){
+  struct trie* f = root;
+  int ind;
+  NA(i,0,s.length()){
+    ind = s[i]-'a';
+    if(!f->cld[ind]) f->cld[ind]=getTrie();
+    f = f->cld[ind];
   }
-  root->children[s[s.length()-1]-'a']->f+=2;
+  f->EndofStr = true;
   return;
 }
-int main(){
-    lli n,q; cin>>n>>q;
-    string s;
-    root = newnode();
-    // NA(i,0,27){
-    //   cout<<root->children[i]->f<<" ";
-    // }
-    NA(i,0,n){
-      cin>>s; insert(s);
-    }
-    NA(i,0,q){
-      cin>>s; query(s);
-    }
+bool search(struct trie* root, string s){
+  struct trie* f = root;
+  int ind;
+  NA(i,0,s.length()){
+    ind = s[i]-'a';
+    if(!f->cld[ind]) return false;
+    f = f->cld[ind];
+  }
+  return (f!=NULL && f->EndofStr);
 }
-
+int main(){
+  struct trie *root = getTrie();
+  int q,p; cin>>q; string s;
+  NA(i,0,q){
+    cin>>p>>s;
+    if(p) insert(root,s);
+    else {
+      if(search(root,s)) cout<<"Exists\n";
+      else cout<<"No\n";
+    } 
+  }
+  return 0;
+}
